8b46a3baaf13db1cca1a0c1e878854d0c7d23655
wyyyyyy <301059120@qq.com>
2018-12-18 21:50:23 +0800

    2018-12-18

---
+++ mtex/components/public/header/searchbar.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="search-panel">
+    <el-row class="m-header-searchbar">
+      <el-col
+        :span="3"
+        class="left">
+        <img
+          src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png"
+          alt="美团">
+      </el-col>
+      <el-col
+        :span="16"
+        class="center">
+        <div class="wrapper">
+          <el-input
+            v-model="search"
+            placeholder="搜索商家或地点"
+          />
+          <button class="el-button el-button--primary"><i class="el-icon-search"/></button>
+          <dl
+            v-if="isHotPlace"
+            class="hotPlace">
+            <dt>热门搜索</dt>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+          </dl>
+          <dl
+            v-if="isSearchList"
+            class="searchList">
+            <dd>热门搜索</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+            <dd>火锅</dd>
+          </dl>
+        </div>
+        <p class="suggest">
+          <a href="#">故宫博物院</a>
+        </p>
+        <ul class="nav">
+          <li><nuxt-link
+            to="/"
+            class="takeout">美团外卖</nuxt-link></li>
+          <li><nuxt-link
+            to="/"
+            class="movie">猫眼电影</nuxt-link></li>
+          <li><nuxt-link
+            to="/"
+            class="hotel">美团酒店</nuxt-link></li>
+          <li><nuxt-link
+            to="/"
+            class="apartment">民宿/公寓</nuxt-link></li>
+          <li><nuxt-link
+            to="/"
+            class="business">商家入驻</nuxt-link></li>
+        </ul>
+      </el-col>
+      <el-col
+        :span="5"
+        class="right">
+        <ul class="security">
+          <li><i class="refund"/><p class="txt">随时退</p></li>
+          <li><i class="single"/><p class="txt">不满意免单</p></li>
+          <li><i class="overdue"/><p class="txt">过期退</p></li>
+        </ul>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+export default {
+  data() {
+    return {
+      search: '',
+      isHotPlace: false,
+      isSearchList: false,
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ mtex/components/public/header/topbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-row 
+    :gutter="0" 
+    class="m-header">
+    <el-col :span="5">
+      <geo/>
+    </el-col>
+    <el-col :span="5">
+      <user/>
+    </el-col>
+    <el-col :span="14">
+      <nav-bar/>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import Geo from './geo.vue'
+import User from './user'
+import NavBar from './nav'
+export default {
+  components: {
+    Geo,
+    User,
+    NavBar
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/css/public/header/index.scss";
+</style>
+
+
