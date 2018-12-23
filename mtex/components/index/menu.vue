9520e456ca5e496b93bef01c3ce110d31acbb55e
wyyyyyy <301059120@qq.com>
2018-12-23 22:05:44 +0800

    2018-12-23

---
+++ mtex/components/index/menu.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="m-menu">
+    <dl class="nav" @mouseleave="leave">
+      <dt>全部分类</dt>
+      <dd v-for="(item, idx) in menu" :key="idx" @mouseenter="enter">
+        <i :class="item.type" />{{item.name}}<span class="arrow"></span>
+      </dd>
+    </dl>
+    <div class="detail" v-if="kind">
+      <template v-for="(item, idx) in curdetail.child">
+        <h4 :key="idx">{{item.title}}</h4>
+        <span v-for="v in item.child" :key="v">{{v}}</span>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        kind: '',
+        menu: [{
+          type: 'food',
+          name: '美食',
+          child: [{
+            title: '外卖',
+            child: ['美团外卖']
+          }]
+        }, {
+          type: 'hotel',
+          name: '酒店',
+          child: [{
+            title: '酒店星级',
+            child: ['经济型', '舒适/三星', '高档/四星', '豪华/五星']
+          }]
+        }, {
+          type: 'food',
+          name: '美食',
+          child: [{
+            title: '当地美食',
+            child: ['烤鸭', '小龙虾', '卤煮', '豆汁']
+          }]
+        }]
+      }
+    },
+    computed: {
+      curdetail: function() {
+        return this.menu.filter(item => item.type === this.kind)[0]
+      }
+    },
+    methods: {
+      leave() {
+        this.kind = ''
+      },
+      enter(e) {
+        this.kind = e.target.querySelector('i').className
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '@/assets/css/index/index.scss';
+</style>
+
+
--- mtex/pages/index.vue
+++ mtex/pages/index.vue
@@ -1,48 +1,24 @@
 <template>
-  <section class="container">
-    
-  </section>
+  <div class="page-index">
+    <el-row>
+      <el-col :span="5">
+        <emenu/>
+      </el-col>
+      <el-col :span="19">2</el-col>
+    </el-row>
+    <el-row :span="24">
+      3
+    </el-row>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import Emenu from '../components/index/menu'
 export default {
   components: {
-    Logo
+    Emenu
   }
 }
 </script>
 
-<style lang="scss">
 
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
