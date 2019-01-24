13ec9cfe37bdf813b5d6414c627500c2287a185c
wyyyyyy <301059120@qq.com>
2019-01-24 22:01:01 +0800

    2019-1-24

--- mtex/server/interface/users.js
+++ mtex/server/interface/users.js
@@ -5,3 +5,53 @@ import User from '../dbs/models/users';
 import Passport from './utils/passport';
 import Email from '../dbs/config';
 import axios from './utils/axios';
+
+let router = new Router({
+  prefix: '/users'
+})
+
+let Store = new Redis().client
+
+router.post('/signup', async (ctx) => {
+  const {
+    username,
+    password,
+    email,
+    code
+  } = ctx.request.body
+
+  if (code) {
+    const saveCode = await Store.hget(`nodemail:${username}`, 'code')
+    const saveExpire = await Store.hget(`nodemail:${username}`, 'expire')
+    if (code === saveCode) {
+      if (new Date().getTime() - saveExpire > 0) {
+        ctx.body = {
+          code: -1,
+          msg: '验证码已过期，请重新获取'
+        }
+        return false
+      }
+    } else {
+      ctx.body = {
+        code: -1,
+        msg: '请填写正确的验证码'
+      }
+    }
+  } else {
+    ctx.body = {
+      code: -1,
+      msg: '请填写验证码'
+    }
+  }
+  let user = await User.find({
+    username
+  })
+  if (user.length) {
+    ctx.body = {
+      code: -1,
+      msg: '已被注册'
+    }
+    return false
+  }
+  
+})
